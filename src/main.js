d8619e188f85cff61a66d9c9ab57560944d60de7
Gustavo Alberto Valladares Sagrero <gvalladares@ucol.mx>
2020-11-26 15:22:26 -0600

    Implemented convertir() function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 export default class App {
 
+    //convertir a pesos
+    convertir(pesos)
+    {
+        let costoDolar = 20.5;
+        let costoEuro = 22.7;
+        let conversion;
+
+        if (pesos < 2000)
+        {
+            conversion = pesos * costoDolar;
+            return conversion;
+        }else if (pesos >= 2000)
+        {
+            conversion = pesos * costoEuro;
+            return conversion;
+        }
+    }
 }
 
 let app = new App();
+
+//Mostrar funciones
+
+console.log(app.convertir(60)); 
+console.log(app.convertir(1000)); 
+console.log(app.convertir(2050)); 
