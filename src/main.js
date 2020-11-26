d4582bf2b9427721aaad93ab02a066f66dc70fbf
Gustavo Alberto Valladares Sagrero <gvalladares@ucol.mx>
2020-11-26 15:30:42 -0600

    Implemented costoRenta() function

--- src/main.js
+++ src/main.js
@@ -17,6 +17,29 @@ export default class App {
             return conversion;
         }
     }
+
+    costoRenta(kilometros)
+    {
+        let costoRenta;
+
+        if (kilometros < 50)
+        {
+            costoRenta = kilometros * 3;
+            return costoRenta;
+        }else if (kilometros >= 50 && kilometros < 100)
+        {
+            costoRenta = kilometros * 5;
+            return costoRenta;
+        }else if (kilometros >= 100 && kilometros < 200)
+        {
+            costoRenta = kilometros * 6;
+            return costoRenta;
+        }else if (kilometros >=200)
+        {
+            costoRenta = kilometros * 6.5;
+            return costoRenta;
+        }
+    }
 }
 
 let app = new App();
@@ -24,5 +47,11 @@ let app = new App();
 //Mostrar funciones
 
 console.log(app.convertir(60)); 
-console.log(app.convertir(1000)); 
 console.log(app.convertir(2050)); 
+
+console.log("costoRenta#######");
+
+console.log(app.costoRenta(44));
+console.log(app.costoRenta(88));
+console.log(app.costoRenta(140));
+console.log(app.costoRenta(203));
